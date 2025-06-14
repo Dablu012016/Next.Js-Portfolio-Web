493c3eb46637d2fba4bfeb6c27ef629719cca3b3
Dablu Kumar <dablukumar3590@gmail.com>
2025-06-15 00:32:28 +0530

    first commit

---
+++ src/contents/project.ts
@@ -0,0 +1,30 @@
+
+import { Project } from "@/types";
+
+export const projects: Project[] = [
+  {
+    title: 'ToDo List',
+    description: 'A frontend technology ToDo List built with React.js and JavaScript',
+    technologies: ['React.js', 'JavaScript', 'CSS', 'HTML'],
+    githubLink: 'https://github.com/Dablu012016/ToDo_App-with-React.Js',
+    demoLink: 'https://demo.com',
+    image: '/projects/ToDo.png',
+  },
+  {
+    title: 'ToDo List',
+    description: 'A ToDo List built with JavaScript, CSS, and HTML.',
+    technologies: ['JavaScript', 'CSS', 'HTML'],
+    githubLink: 'https://github.com/Dablu012016/ToDo_App',
+    demoLink: 'https://demo.com',
+    image: '/projects/ToDo_Js.png',
+  },
+  {
+    title: 'Portfolio Website',
+    description: 'My personal portfolio website showcasing my projects and skills.',
+    technologies: ['Next.js', 'Tailwind CSS', 'JavaScript', 'Framer Motion'],
+    githubLink: 'https://github.com/Dablu012016/Next.Js-Portfolio-Web.git',
+    demoLink: 'https://demo.com',
+    image: '/projects/PortFolio.png',
+  },
+
+];
